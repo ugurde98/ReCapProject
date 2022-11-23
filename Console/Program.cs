a738c8e6124e6cd72d5999581c607d45bfa1579e
ugurde98 <ugurde98@gmail.com>
2022-11-23 10:56:23 +0300

    Proje dosyası ekle.

---
+++ Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            foreach (var item in carManager.GetAll())
+            {
+                Console.WriteLine(item.Description);
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,48 @@
+﻿using DataAccess.Abtract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _car;
+        public InMemoryCarDal() 
+        {
+            _car = new List<Car> 
+            { 
+                new Car {Id=7,BrandId=3,ColorId=2,DailyPrice=200,Description="Renault Clio",ModelYear="2015" },
+                new Car {Id=5,BrandId=2,ColorId=2,DailyPrice=155,Description="fiat egea",ModelYear="2010" } 
+            };
+        
+        }
+        public void Add(Car car)
+        {
+            _car.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car CarDelete=_car.SingleOrDefault(x => x.Id==car.Id);
+            _car.Remove(CarDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _car;
+        }
+
+        public void Update(Car car)
+        {
+            Car CarUpdate=_car.SingleOrDefault(x => x.Id==car.Id);
+            CarUpdate.DailyPrice=car.DailyPrice;
+            CarUpdate.Description=car.Description;
+
+
+        }
+    }
+}
